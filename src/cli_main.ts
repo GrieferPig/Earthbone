7dfca1d4fc1e29d63df7bf8e4cbb4a5f2a0e03a0
GrieferPig <grieferpig@163.com>
2022-06-05 18:51:38 +0800

    feat: 添加主入口文件&参数判断

--- src/cli_main.ts
+++ src/cli_main.ts
@@ -1,6 +1,46 @@
-import {Earthbone} from "./index";
+const args = process.argv.slice(2)
 
-console.log("hi")
+if (args.length == 0) {
+    printUsage()
+    process.exit(1)
+}
 
-const e = new Earthbone()
-e.sayHi('bruh').sayHi('there yo go').sayHi();
+switch (args[0]) {
+    case "-h":
+        printUsage();
+        break;
+
+    case "moo":
+        console.log('Don\'t even think about it.')
+        break;
+
+    case "-v":
+        printVer("b", "e");
+        break;
+
+    case "-vv":
+        printVer("p", "e");
+        break;
+
+    case "-vvv":
+        printVer("p", "y");
+        break;
+
+    default:
+        printUsage()
+}
+
+function printUsage() {
+    console.log([
+        "Usage: Earthbone.exe [args]",
+        "Options:",
+        "   -v      Print Earthbone's version",
+        "   -h      Print this help message ",
+        "",
+        "                   This Earthbone does not have super cow powers."
+    ].join("\n"))
+}
+
+function printVer(egg: string, egg1: string) {
+    console.log(`Earth${egg}on${egg1} v0.0.3`);
+}
